e868da13d1f1cba5ef8a1b2f69bd1eb4917c7e70
KeeranNaidu12 <144285833+KeeranNaidu12@users.noreply.github.com>
2024-08-13 17:24:29 -0700

    Update password.py
    I changed my while loop to using the random method to generate  the characters

--- password.py
+++ password.py
@@ -11,20 +11,38 @@ capsNo = tk.IntVar()
 specialNo = tk.IntVar()
 numbersNo = tk.IntVar()
 
-lower = lowerNo.get()
-caps = capsNo.get()
-special = specialNo.get()
-numbers = numbersNo.get()
-sum = 0
+# Building the password using these conditions
+def randomize():
+    lower = lowerNo.get()
+    caps = capsNo.get()
+    special = specialNo.get()
+    numbers = numbersNo.get()
+    counter = lower + caps + special + numbers
+
+    if counter > 20:
+        generated.config(text=" The total characetrs selcted exceeds the password limit! Try again")
+        return
+
+    char = []
+
+    char.extend(random.choice("abcdefghijklmnopqrstuvwxyz",k=lower))
+    char.extend(random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ",k=caps))
+    char.extend(random.choice("0123456789",k=numbers))
+    char.extend(random.choice("+-=*&^%$#@!?><",k=special))
+
+    random.shuffle(char)
+    password = ''.join(char)
+
+    button.config(text="Your password is: " + password)
+
+
 
 # Window title and Dimensions
 base.title("Python Password Generator")
 base.geometry("650x1600")
 
 
-
-
-
+# Labels with what the program is about and basic instructions
 welcome = tk.Label(base,text = "Welcome to my randomized password generator!", font=('arial',20,'bold'))
 welcome.grid(row=1,column=1, sticky= tk.N,pady=20, padx=80)
 
@@ -41,81 +59,30 @@ option1L.grid(row=30,column=1, pady=10,sticky=tk.W, padx= 200)
 option1E = tk.Entry(base,textvariable = lowerNo, font=('arial',10))
 option1E.grid(row=30,column=1, pady=10, sticky=tk.W, padx = 300)
 
-option1L = tk.Label(base,text = "Captials")
-option1L.grid(row=40,column=1, pady=10,sticky=tk.W, padx= 200)
+option2L = tk.Label(base,text = "Captials")
+option2L.grid(row=40,column=1, pady=10,sticky=tk.W, padx= 200)
 
-option1E = tk.Entry(base,textvariable = capsNo, font=('arial',10))
-option1E.grid(row=40,column=1, sticky=tk.W,pady=10, padx = 300)
+option2E = tk.Entry(base,textvariable = capsNo, font=('arial',10))
+option2E.grid(row=40,column=1, sticky=tk.W,pady=10, padx = 300)
 
-option1L = tk.Label(base,text = "Numbers")
-option1L.grid(row=50,column=1, pady=10,sticky=tk.W, padx= 200)
+option3L = tk.Label(base,text = "Numbers")
+option3L.grid(row=50,column=1, pady=10,sticky=tk.W, padx= 200)
 
-option1E = tk.Entry(base,textvariable = numbersNo, font=('arial',10))
-option1E.grid(row=50,column=1, sticky=tk.W,padx = 300, pady=10)
+option3E = tk.Entry(base,textvariable = numbersNo, font=('arial',10))
+option3E.grid(row=50,column=1, sticky=tk.W,padx = 300, pady=10)
 
-option1L = tk.Label(base,text = "Special")
-option1L.grid(row=60,column=1, pady=10,sticky=tk.W, padx= 200)
+option4L = tk.Label(base,text = "Special")
+option4L.grid(row=60,column=1, pady=10,sticky=tk.W, padx= 200)
 
-option1E = tk.Entry(base,textvariable = specialNo, font=('arial',10))
-option1E.grid(row=60,column=1, sticky=tk.W,pady=10, padx = 300)
+option4E = tk.Entry(base,textvariable = specialNo, font=('arial',10))
+option4E.grid(row=60,column=1, sticky=tk.W,pady=10, padx = 300)
 
-def summation(sum):
-    sum = lower + caps + special + numbers
-    
-# Building the password using these conditions
-def randomize(sum):
-    counter = 0
-    while(sum > counter):
-        choice = random.randint(1,4)
-        if option1.lower() == "yes" and choice == 1:
-            password += chr(random.randint(33,47))   
-        elif option2 == "yes" and choice == 2:
-            password += chr(random.randint(48,57))
-        elif option3 == "yes" and choice == 3:
-            password += chr(random.randint(65,90))
-        else:
-            password += chr(random.randint(97,122))
-        
-    counter+=1
+# Button to generate the password
+button = tk.Button(base, text = "Generate a Password",command=randomize)
+button.grid(row=90,column=1, sticky=tk.W,pady=10, padx = 300)
 
-
-if(summation(sum) > 0 and summation(sum) < 20):
-    randomize(sum)
-
-
+# Display message indicating the password generation
+generated = tk.Label(base, text="")
+generated.grid(row=110,column=1, sticky=tk.W,pady=10, padx = 300)
 
 base.mainloop()
-
-#---------START-OF-PROGRAM---------------------------------------------------------------------------
-
-password = ""
-
-
-print("Got it! Your password will be ", value ," characters long.")
-
-# Special Characters?
-print("Would you like to have special characters (like *,+,!, etc.) in your password? (Yes/No)")
-option1 = input()
-
-if(yesCheck(option1) == True):
-    print("We will consider this!")
-else: 
-    print("We will not consider this!")
-
-# Numbers?
-print("Would you like to have numbers in your password? (Yes/No)")
-option2 = input()
-
-if(yesCheck(option2) == True):
-    print("We will consider this!")
-else: 
-    print("We will not consider this!")
-
-# Captial Letters?
-print("Would you like to have capital letters in your password? (Yes/No)")
-option3 = input()
-
-if(yesCheck(option3) == True):
-    print("We will consider this!")
-else: 
-    print("We will not consider this!")
